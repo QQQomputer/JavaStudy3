f86f43c63a2dce92932251292fc04a724645d455
MoonJH <3477qq@gmail.com>
2022-01-04 15:34:32 +0900

    add Rs

--- src/com/gom/s1/method/MethodMain.java
+++ src/com/gom/s1/method/MethodMain.java
@@ -3,8 +3,25 @@ package com.gom.s1.method;
 public class MethodMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		System.out.println("MainMethod Start");
+
+		//객체 생성 코드
+		//new 클래스명();
+		ReturnStudy rs = new ReturnStudy();
+		
+		//참조변수명.멤버들
+		rs.test1();
+	
+		int count = rs.test2();
+		System.out.println("Main : "+count);
+		
+		String name = rs.test3();
+		System.out.println(name);
+		
+		double a = Math.random();
+		System.out.println(a);
+		
+		System.out.println("MainMethod Finish");
 	}
-
 }
